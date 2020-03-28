8af006b379cec190b80215535061c63c626b763e
IcarusTheFly <IcarusTheFly@users.noreply.github.com>
2020-03-28 18:31:16 +0100

    Rename Sorted Union to Sorted Union.js

--- Javascript/Intermediate Algorithm Scripting/Sorted Union.js
+++ Javascript/Intermediate Algorithm Scripting/Sorted Union.js
@@ -1,4 +1,4 @@
-function uniteUnique(...arr) {
+function uniteUnique( ...arr ) {
   var result = [];
   for ( var i = 0; i < arr.length; i++ ) {
     for ( var j = 0; j < arr[ i ].length; j++ ) {
@@ -10,4 +10,4 @@ function uniteUnique(...arr) {
   return result;
 }
 
-uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+uniteUnique( [ 1, 3, 2 ], [ 5, 2, 1, 4 ], [ 2, 1 ] );
